3889f3447e86e7802a4028b50078ef9e1e90dd65
venketesh-mech <71964830+venketesh-mech@users.noreply.github.com>
2021-08-30 22:25:54 +0530

    Add files via upload

---
+++ MovieManager/src/Movie Manager-29-AUG-2021/Movie Manager/LoadJSON.js
@@ -0,0 +1,13 @@
+// global variable
+var data;
+
+$.get('Sample.json', function(d) {
+    data = JSON.parse(d);
+    // loop through all movies
+    alert(data);
+    data.movies.forEach(function(b) {
+        // now you can put every movie in your <div>
+
+        $("#Movie-Grid").append(`<div class=\"MovieCard\"><div class=\"Mov-Img-Container\"><div class=\"Movie-Img\"><img src = \" ${b.Title}\" title=${b.Title} width=\"180px\"  ></div></div><div class=\"Movie-Name\">${b.Title}</div><div class=\"Movie-Year\">${b.Year}</div></div>`);
+    });
+});
